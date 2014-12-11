a8f1803d0dd38d855e911170f42a0dcd24bd44ce
Chaow Porkaew <pchaowmobile@gmail.com>
2014-12-11 10:34:33 +0700

    add news service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,40 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope,News) {
-    $scope.newsList = News.all();
+    .controller('HomeCtrl', function ($scope, News) {
+        $scope.newsList = [];
 
-})
+        News.all()
+            .success(function (response) {
+                $scope.newsList = response.data;
+            })
+            .error(function (response) {
+                alert("error");
+            })
 
-.controller('NewsDetailCtrl', function($scope,News,$stateParams) {
+    })
+
+    .controller('NewsDetailCtrl', function ($scope, News, $stateParams) {
         $id = $stateParams.newsId;
-        $scope.news = News.get($id);
-})
+        $scope.news = {};
 
-.controller('ProjectDetailCtrl', function($scope) {
-})
+        News.get($id)
+            .success(function (response) {
+                $scope.news = response.data;
+                console.log(response);
+            })
+    })
 
-.controller('ProjectCtrl', function($scope) {
-})
+    .controller('ProjectDetailCtrl', function ($scope) {
+    })
 
-.controller('MapCtrl', function($scope) {
-})
+    .controller('ProjectCtrl', function ($scope) {
+    })
 
-.controller('MediaCtrl', function($scope) {
-})
+    .controller('MapCtrl', function ($scope) {
+    })
 
-.controller('ContactCtrl', function($scope) {
-});
+    .controller('MediaCtrl', function ($scope) {
+    })
+
+    .controller('ContactCtrl', function ($scope) {
+    });
--- www/js/services.js
+++ www/js/services.js
@@ -1,45 +1,56 @@
 angular.module('starter.services', [])
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
 
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-})
+    .factory('Base',function($http){
 
-.factory('News',function($http){
-        var news = [
-            { id:0, header : "Header 1", content : "Content 1", news_image : 'holder.js/80x80'},
-            { id:1, header : "Header 2", content : "Content 2", news_image : 'holder.js/80x80'},
-            { id:2, header : "Header 3", content : "Content 3", news_image : 'holder.js/80x80'},
-            { id:3, header : "Header 4", content : "Content 4", news_image : 'holder.js/80x80'},
-            { id:4, header : "Header 5", content : "Content 5", news_image : 'holder.js/80x80'},
+        BASE_URL = 'http://localhost:8000/api/v1/';
+
+        return {
+            getBaseUrl : BASE_URL,
+            resolveUrl : function(uri){
+                return BASE_URL + uri;
+            }
+        }
+    })
+
+    .factory('Friends', function () {
+        // Might use a resource here that returns a JSON array
+
+        // Some fake testing data
+        var friends = [
+            {id: 0, name: 'Scruff McGruff'},
+            {id: 1, name: 'G.I. Joe'},
+            {id: 2, name: 'Miss Frizzle'},
+            {id: 3, name: 'Ash Ketchum'}
         ];
 
         return {
-            all : function(){
-                return news;
+            all: function () {
+                return friends;
             },
-            get : function($id){
-                return news[$id];
+            get: function (friendId) {
+                // Simple index lookup
+                return friends[friendId];
+            }
+        }
+    })
+
+    .factory('News', function ($http,Base) {
+
+        return {
+            all: function () {
+                return $http({
+                    url : Base.resolveUrl('news'),
+                    method : 'get'
+                });
+            },
+            get: function ($id) {
+                return $http({
+                    url : Base.resolveUrl('news/views/'+$id),
+                    method : 'get'
+
+                })
             }
         }
     });
